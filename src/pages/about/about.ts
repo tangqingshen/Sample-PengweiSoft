0b56c768d4f2a34d00161bab68ccf73fe28f3c8f
Jacky Tse <megadron@qq.com>
2017-02-10 15:15:08 +0800

    修改 About 页面，增加应用名和版本号

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { AppVersion } from 'ionic-native';
 
 @Component({
   selector: 'page-about',
@@ -8,8 +8,22 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  public appName: String;
+  public version: String;
 
+  constructor(public navCtrl: NavController, public platform: Platform) {
+    if (this.platform.is("mobileweb") || this.platform.is("core")) {
+      this.appName = "null";
+      this.version = "null";
+    } else {
+      AppVersion.getAppName().then((name: String)=>{
+        this.appName = name;
+      });
+
+      AppVersion.getVersionCode().then((version: String)=>{
+        this.version = version;
+      });
+    }
   }
 
 }
